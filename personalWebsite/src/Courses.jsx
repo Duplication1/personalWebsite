aedb939e96013d105077d64dd2e9af9c68032503
Duplication1 <gamot.kim.fernandez@gmail.com>
2024-07-17 11:00:37 +0800

    added certificate and attainment

--- personalWebsite/src/Courses.jsx
+++ personalWebsite/src/Courses.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import './App.css'
 const Courses = () => {
     return (
-        <div className="courses-container">
-            <h2>Courses Component</h2>
-            {/* Add your Courses content here */}
+        <div className='projects-body'>
+           <h1 className='h1-project'>Certificates</h1>
+            <div className='projects-container'>
+            <a href='https://www.freecodecamp.org/fcc7da37b8c-a266-41b9-b555-42ff0d5c35ea' target='_blank'><div className="projects"><img src='./ResponsiveWebDesign-Certification.png'/><p>Responsive Web Design</p></div></a>
+            <a href='https://www.facebook.com/story.php/?story_fbid=223427070775381&id=100093242695765' target='_blank'><div className="projects"><img src='./234.png'/><p>IGNITE Summit 2023</p></div></a>
+            </div>
         </div>
     );
 }
--- personalWebsite/src/Education.jsx
+++ personalWebsite/src/Education.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import './App.css'
 const Education = () => {
     return (
-        <div className="education-container">
-            <h2>education Component</h2>
-            {/* Add your resume content here */}
+        <div className='projects-body'>
+           <h1 className='h1-project'>Education</h1>
+            <div className='projects-container'>
+            <a href='https://www.seaitt.com/' target='_blank'><div className="projects"><img src='./shs.jpg'/><p>South-East Asia Institute of Trade and Technology</p></div></a>
+            <a href='https://qcu.edu.ph/' target='_blank'><div className="projects"><img src='./shs.jpg'/><p>Quezon City University</p></div></a>
+            </div>
         </div>
     );
 }
